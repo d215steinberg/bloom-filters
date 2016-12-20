584aba7703cb3f3affe8ea1ff462b41f76f49ac5
d215steinberg <daniel.steinberg@cengage.com>
2016-12-20 15:12:45 -0600

    Red test:  setBit does not exist.

--- specs/bloomFiltersSpec.js
+++ specs/bloomFiltersSpec.js
@@ -1,8 +1,16 @@
 describe('Bloom Filters', function() {
 	var expect = require('chai').expect;
 	var bloomFilters = require('../bloomFilters');
+	var sinon = require('sinon');
 
 	it('should look up a word', function() {
 		expect(bloomFilters.lookup).to.be.a('function');
+	});
+
+	it('should recognize a word whose single hash is a hit', function() {
+		bloomFilters.setBit(17);
+		sinon.stub(hasher, 'getHash').withArgs('foo').returns(17);
+
+		expect(bloomFilters.lookup('foo')).to.be(true);
 	});
 });
