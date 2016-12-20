4af1ffcd2b7d353dea66286c858277e1c0037eb0
d215steinberg <daniel.steinberg@cengage.com>
2016-12-20 15:31:03 -0600

    Red test: Simplest miss case

--- specs/bloomFiltersSpec.js
+++ specs/bloomFiltersSpec.js
@@ -2,6 +2,11 @@ describe('Bloom Filters', function() {
 	var expect = require('chai').expect;
 	var bloomFilters = require('../bloomFilters');
 	var sinon = require('sinon');
+	var hasher = require('../hasher');
+
+	before(function() {
+		sinon.stub(hasher, 'getHash').withArgs('foo').returns(17);
+	});
 
 	it('should look up a word', function() {
 		expect(bloomFilters.lookup).to.be.a('function');
@@ -9,8 +14,10 @@ describe('Bloom Filters', function() {
 
 	it('should recognize a word whose single hash is a hit', function() {
 		bloomFilters.setBit(17);
-		sinon.stub(hasher, 'getHash').withArgs('foo').returns(17);
+		expect(bloomFilters.lookup('foo')).to.be.true;
+	});
 
-		expect(bloomFilters.lookup('foo')).to.be(true);
+	it('should not recognize a word whose single hash is a miss', function() {
+		expect(bloomFilters.lookup('foo')).to.be.false;
 	});
 });
