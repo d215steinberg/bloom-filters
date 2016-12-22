96158b4ce131bd616b6b9705d65d90a71832b09a
d215steinberg <d215steinberg@yahoo.com>
2016-12-22 09:53:42 -0600

    Moved common code into beforeEach blocks

--- specs/bloomFiltersSpec.js
+++ specs/bloomFiltersSpec.js
@@ -16,18 +16,15 @@ describe('Bloom Filters', function () {
 	describe('Single hash function', function () {
 		beforeEach(function () {
 			hasher.NUM_HASHES = 1;
+			hasher.getHash.withArgs('foo', 1).returns(17);
 		});
 
 		it('should recognize a word whose single hash is a hit', function () {
-			hasher.getHash.withArgs('foo', 1).returns(17);
 			bitArray.setBit(17);
-
 			expect(bloomFilters.lookup('foo')).to.be.true;
 		});
 
 		it('should not recognize a word whose single hash is a miss', function () {
-			hasher.NUM_HASHES = 1;
-			hasher.getHash.withArgs('foo', 1).returns(17);
 			expect(bloomFilters.lookup('foo')).to.be.false;
 		});
 	});
@@ -35,12 +32,12 @@ describe('Bloom Filters', function () {
 	describe('Multiple hash functions', function () {
 		beforeEach(function() {
 			hasher.NUM_HASHES = 3;
-		});
-
-		it('should not recognize a word for which one of multiple hashes is a miss', function () {
 			hasher.getHash.withArgs('foo', 1).returns(15);
 			hasher.getHash.withArgs('foo', 2).returns(17);
 			hasher.getHash.withArgs('foo', 3).returns(19);
+		});
+
+		it('should not recognize a word for which one of multiple hashes is a miss', function () {
 			bitArray.setBit(15);
 			bitArray.setBit(19);
 
@@ -48,9 +45,6 @@ describe('Bloom Filters', function () {
 		});
 
 		it('should recognize a word for which all of multiple hashes are hits', function () {
-			hasher.getHash.withArgs('foo', 1).returns(15);
-			hasher.getHash.withArgs('foo', 2).returns(17);
-			hasher.getHash.withArgs('foo', 3).returns(19);
 			bitArray.setBit(15);
 			bitArray.setBit(17);
 			bitArray.setBit(19);
