b46cee3f3c4888889ebc47bf717002440de82156
d215steinberg <d215steinberg@yahoo.com>
2016-12-22 09:56:38 -0600

    Extracted method isHashAMiss

--- bloomFilters.js
+++ bloomFilters.js
@@ -2,9 +2,13 @@
 	var bitArray = require('./bitArray');
 	var hasher = require('./hasher');
 
+	function isHashAMiss(word, hashNum) {
+		return bitArray.getBit(hasher.getHash(word, hashNum)) != 1;
+	}
+
 	module.exports.lookup = function (word) {
 		for (var hashNum = 1; hashNum <= hasher.NUM_HASHES; hashNum++) {
-			if (bitArray.getBit(hasher.getHash(word, hashNum)) != 1) {
+			if (isHashAMiss(word, hashNum)) {
 				return false;
 			}
 		}
