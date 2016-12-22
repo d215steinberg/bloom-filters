51ba2401a4ba91709ada47736697bd5e0d8e37a6
d215steinberg <d215steinberg@yahoo.com>
2016-12-22 09:49:12 -0600

    Broke tests out into multiple describe-blocks

--- specs/bloomFiltersSpec.js
+++ specs/bloomFiltersSpec.js
@@ -1,52 +1,61 @@
-describe('Bloom Filters', function() {
+describe('Bloom Filters', function () {
 	var expect = require('chai').expect;
+	var sinon = require('sinon');
 	var bloomFilters = require('../bloomFilters');
 	var bitArray = require('../bitArray');
-	var sinon = require('sinon');
 	var hasher = require('../hasher');
 
-	before(function() {
+	before(function () {
 		sinon.stub(hasher, 'getHash');
 	});
 
-	beforeEach(function() {
+	beforeEach(function () {
 		bitArray.clear();
 	});
 
-	it('should recognize a word whose single hash is a hit', function() {
-		hasher.NUM_HASHES = 1;
-		hasher.getHash.withArgs('foo', 1).returns(17);
-		bitArray.setBit(17);
+	describe('Single hash function', function () {
+		beforeEach(function () {
+			hasher.NUM_HASHES = 1;
+		});
 
-		expect(bloomFilters.lookup('foo')).to.be.true;
+		it('should recognize a word whose single hash is a hit', function () {
+			hasher.getHash.withArgs('foo', 1).returns(17);
+			bitArray.setBit(17);
+
+			expect(bloomFilters.lookup('foo')).to.be.true;
+		});
+
+		it('should not recognize a word whose single hash is a miss', function () {
+			hasher.NUM_HASHES = 1;
+			hasher.getHash.withArgs('foo', 1).returns(17);
+			expect(bloomFilters.lookup('foo')).to.be.false;
+		});
 	});
 
-	it('should not recognize a word whose single hash is a miss', function() {
-		hasher.NUM_HASHES = 1;
-		hasher.getHash.withArgs('foo', 1).returns(17);
-		expect(bloomFilters.lookup('foo')).to.be.false;
-	});
+	describe('Multiple hash functions', function () {
+		beforeEach(function() {
+			hasher.NUM_HASHES = 3;
+		});
 
-	it('should not recognize a word for which one of multiple hashes is a miss', function() {
-		hasher.NUM_HASHES = 3;
-		hasher.getHash.withArgs('foo', 1).returns(15);
-		hasher.getHash.withArgs('foo', 2).returns(17);
-		hasher.getHash.withArgs('foo', 3).returns(19);
-		bitArray.setBit(15);
-		bitArray.setBit(19);
+		it('should not recognize a word for which one of multiple hashes is a miss', function () {
+			hasher.getHash.withArgs('foo', 1).returns(15);
+			hasher.getHash.withArgs('foo', 2).returns(17);
+			hasher.getHash.withArgs('foo', 3).returns(19);
+			bitArray.setBit(15);
+			bitArray.setBit(19);
 
-		expect(bloomFilters.lookup('foo')).to.be.false;
-	});
+			expect(bloomFilters.lookup('foo')).to.be.false;
+		});
 
-	it('should recognize a word for which all of multiple hashes are hits', function() {
-		hasher.NUM_HASHES = 3;
-		hasher.getHash.withArgs('foo', 1).returns(15);
-		hasher.getHash.withArgs('foo', 2).returns(17);
-		hasher.getHash.withArgs('foo', 3).returns(19);
-		bitArray.setBit(15);
-		bitArray.setBit(17);
-		bitArray.setBit(19);
+		it('should recognize a word for which all of multiple hashes are hits', function () {
+			hasher.getHash.withArgs('foo', 1).returns(15);
+			hasher.getHash.withArgs('foo', 2).returns(17);
+			hasher.getHash.withArgs('foo', 3).returns(19);
+			bitArray.setBit(15);
+			bitArray.setBit(17);
+			bitArray.setBit(19);
 
-		expect(bloomFilters.lookup('foo')).to.be.true;
+			expect(bloomFilters.lookup('foo')).to.be.true;
+		});
 	});
 });
