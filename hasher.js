f08f689ed2b30f548fc864dd1bdaa699b119f590
d215steinberg <d215steinberg@yahoo.com>
2016-12-22 22:21:45 -0600

    Cleaned up hasher.

--- hasher.js
+++ hasher.js
@@ -1,9 +1,15 @@
-var darkSkyAppStringHash = require('string-hash');
-var hashFunctions = [darkSkyAppStringHash];
+(function() {
+	var darkSkyAppStringHash = require('string-hash');
+	var djb2ish = function (word) {
+		return Math.floor(darkSkyAppStringHash(word) / 8);
+	};
+	
+	var hashFunctions = [djb2ish];
 
-module.exports.NUM_HASHES = hashFunctions.length;
+	module.exports.NUM_HASHES = hashFunctions.length;
 
-module.exports.getHash = function(word, hashNum){
-	return Math.floor(hashFunctions[hashNum - 1](word) / 8);
-};
+	module.exports.getHash = function (word, hashNum) {
+		return hashFunctions[hashNum - 1](word);
+	};
+})();
 
