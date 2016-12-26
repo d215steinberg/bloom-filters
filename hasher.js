466fd8c2962691b82ff3b87f43e4d48e50b51c17
d215steinberg <d215steinberg@yahoo.com>
2016-12-25 18:37:44 -0600

    3rd hash:  Derived from md5

--- hasher.js
+++ hasher.js
@@ -3,8 +3,22 @@
 	var djb2ish = function (word) {
 		return Math.floor(darkSkyAppStringHash(word) / 8);
 	};
-	
-	var hashFunctions = [djb2ish];
+
+	var sha1 = require('node-sha1');
+	var sha1Hash = function(word) {
+		var hash = sha1(word);
+		var last7Digits = hash.substr(hash.length - 7);
+		return parseInt(last7Digits, 16);
+	};
+
+	var crypto = require('crypto');
+	var md5Hash = function(word) {
+		var hash = crypto.createHash('md5').update(word).digest("hex");
+		var last7Digits = hash.substr(hash.length - 7);
+		return parseInt(last7Digits, 16);
+	};
+
+	var hashFunctions = [djb2ish, sha1Hash, md5Hash];
 
 	module.exports.NUM_HASHES = hashFunctions.length;
 
