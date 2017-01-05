bca66abcbcbff39bc143152f600da715c5d62fff
d215steinberg <d215steinberg@yahoo.com>
2017-01-05 14:43:10 -0600

    RED: First test for random lower case letter generator

---
+++ specs/randomLowerCaseLetterGeneratorSpec.js
@@ -0,0 +1,15 @@
+describe('Random lower case letter generator', function() {
+	var sinon = require('sinon');
+	var expect = require('chai').expect;
+	var _ = require('underscore');
+	var randomLowerCaseLetterGenerator = require('../randomLowerCaseLetterGenerator');
+
+	before(function() {
+		sinon.stub(_, 'random');
+	});
+
+	it("should generate 'a' as lowest value", function() {
+		_.random.withArgs(97, 122).returns(97);
+		expect(randomLowerCaseLetterGenerator.generate()).to.equal('a');
+	});
+});
