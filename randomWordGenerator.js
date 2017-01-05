8b88c9971adeaec66777e8d454020bcb2314f4c5
d215steinberg <d215steinberg@yahoo.com>
2017-01-05 14:06:15 -0600

    GREEN:  Random word generator spec

--- randomWordGenerator.js
+++ randomWordGenerator.js
@@ -1,4 +1,12 @@
 (function () {
+	var randomLowerCaseLetterGenerator = require('./randomLowerCaseLetterGenerator');
+
 	module.exports.generate = function() {
+		var word = "";
+		for (var i = 0; i < 5; i++) {
+			word += randomLowerCaseLetterGenerator.generate();
+		}
+
+		return word;
 	};
 })();
--- specs/randomWordGeneratorSpec.js
+++ specs/randomWordGeneratorSpec.js
@@ -1,11 +1,11 @@
 describe('Random Word Generator', function() {
 	var sinon = require('sinon');
 	var expect = require('chai').expect;
+	var randomLowerCaseLetterGenerator = require('../randomLowerCaseLetterGenerator');
 	var randomWordGenerator = require('../randomWordGenerator');
-	var randomLowercaseLetterGenerator = require('../randomLowercaseLetterGenerator');
 
 	it('should generate a sequence of 5 random lower-case letters', function() {
-		sinon.stub(randomLowercaseLetterGenerator, 'generate')
+		sinon.stub(randomLowerCaseLetterGenerator, 'generate')
 			.onCall(0).returns('a')
 			.onCall(1).returns('z')
 			.onCall(2).returns('m')
