d85b9d0f756486fa14e9f0afe5ac3766ad1fb1fb
d215steinberg <d215steinberg@yahoo.com>
2017-01-24 13:55:58 -0600

    GREEN:  Should find word in latter part of list

--- binaryDictionary.js
+++ binaryDictionary.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
 	var rp = require('request-promise');
 	var _s = require('underscore.string');
 	var q = require('q');
 	var words;
 
-	module.exports.getWords = function() {
+	module.exports.getWords = function () {
 		return words;
 	};
 
-	module.exports.loadDictionary = function(dictionarySource) {
+	module.exports.loadDictionary = function (dictionarySource) {
 		return rp(dictionarySource)
-			.then(function(data) {
+			.then(function (data) {
 				words = _s.words(data);
 			})
-			.catch(function(error) {
+			.catch(function (error) {
 				return q.reject(error);
 			});
 	};
@@ -22,13 +22,17 @@
 		if (wordList.length === 0) {
 			return false;
 		}
+		if (wordList.length === 1) {
+			return wordList[0] === word;
+		}
 
 		var middlePosition = Math.floor(wordList.length / 2);
 		return wordList[middlePosition] === word
-			|| lookup(wordList.slice(0, middlePosition), word);
+			|| lookup(wordList.slice(0, middlePosition), word)
+			|| lookup(wordList.slice(middlePosition, wordList.length), word);
 	}
 
-	module.exports.lookup = function(word) {
+	module.exports.lookup = function (word) {
 		return lookup(words, word);
 	};
 })();
